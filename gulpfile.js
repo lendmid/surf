38382099884bbfa0306925398104eeccc90f1102
Dmitriy <lendmid@bk.ru>
2022-08-08 00:55:07 +0300

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,65 @@
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    concat = require('gulp-concat'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const exec = require('gulp-exec');
 
 
-gulp.task('clean', async function(){
-  del.sync('build')
-})
+gulp.task('html', () => gulp.src('src/*.html')
+    .pipe(browserSync.reload({stream: true})));
 
-gulp.task('scss', function(){
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 8 versions']
-    }))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('css', function(){
-  return gulp.src([
+gulp.task('css', () => gulp.src([
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/normalize.css/normalize.css',
     'node_modules/animate.css/animate.css',
-  ])
+])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('src/scss'))
-    .pipe(browserSync.reload({stream: true}))
-});
+    .pipe(browserSync.reload({stream: true})));
 
-gulp.task('html', function(){
-  return gulp.src('src/*.html')
-  .pipe(browserSync.reload({stream: true}))
-});
+gulp.task('scss', () => gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 8 versions']
+    }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({stream: true})));
 
-gulp.task('script', function(){
-  return gulp.src('src/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
-});
+gulp.task('script', () => gulp.src('src/js/*.js')
+    .pipe(browserSync.reload({stream: true})));
 
-gulp.task('js', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
-  ])
-    .pipe(concat('libs.min.js'))
+
+gulp.task('clean', async () => exec(() => `(if exist build rmdir /Q /S build)`));
+
+gulp.task('js', () => gulp.src('node_modules/slick-carousel/slick/slick.js')
+    .pipe(concat('slick.js'))
     .pipe(gulp.dest('src/js'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({stream: true})));
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
 });
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "src/"
-      }
-  });
+gulp.task('export', async () => {
+    gulp.src('src/**/*.html').pipe(gulp.dest('build'));
+    gulp.src('src/css/**/*.css').pipe(gulp.dest('build/css'));
+    gulp.src('src/js/**/*.js').pipe(gulp.dest('build/js'));
+    gulp.src('src/fonts/**/*.*').pipe(gulp.dest('build/fonts'));
+    gulp.src('src/img/**/*.*').pipe(gulp.dest('build/img'));
 });
 
-gulp.task('export', function(){
-  let buildHtml = gulp.src('src/**/*.html')
-    .pipe(gulp.dest('build'));
-
-  let BuildCss = gulp.src('src/css/**/*.css')
-    .pipe(gulp.dest('build/css'));
-
-  let BuildJs = gulp.src('src/js/**/*.js')
-    .pipe(gulp.dest('build/js'));
-    
-  let BuildFonts = gulp.src('src/fonts/**/*.*')
-    .pipe(gulp.dest('build/fonts'));
-
-  let BuildImg = gulp.src('src/img/**/*.*')
-    .pipe(gulp.dest('build/img'));   
+gulp.task('watch', () => {
+    gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('src/*.html', gulp.parallel('html'))
+    gulp.watch('src/js/*.js', gulp.parallel('script'))
 });
 
-gulp.task('watch', function(){
-  gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('src/*.html', gulp.parallel('html'))
-  gulp.watch('src/js/*.js', gulp.parallel('script'))
-});
+//
+gulp.task('start', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
 
 gulp.task('build', gulp.series('clean', 'export'))
-
-gulp.task('start', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
